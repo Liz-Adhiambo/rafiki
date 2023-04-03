1f8d3a0c9dea1e18d42884aa7f38919b3349bf67
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2023-04-03 10:47:27 +0300

    deploying to railway

--- rafiki_project/settings.py
+++ rafiki_project/settings.py
@@ -51,8 +51,7 @@ DATABASES = {
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
